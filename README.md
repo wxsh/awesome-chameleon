0b2c60b6524288ecd6a6bf3862c69481cb9372ca
Ocularis <andreas.ocularis@gmail.com>
2017-01-25 13:05:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+I use feh on xinit, so it probably will not work with the current wallpaper setup without feh in xinitrc. 
+
+Wallpaper is at http://ether.deviantart.com/art/sliver-160987836
---
+++ themes/chameleon/theme.lua
@@ -0,0 +1,112 @@
+
+--[[
+	Chameleon theme by Ocularis
+	Originally bsaed off multicolor by copycat-killer.
+--]]
+
+
+local theme                                     = {}
+
+theme.confdir                                   = os.getenv("HOME") .. "/.config/awesome/themes/chameleon"
+theme.wallpaper	                                = "eval 'sh ~/.fehbg'"
+
+theme.font                                      = "artwiz lemon 8"
+--local colors = require("colorsForTheme.lua")
+theme.menu_bg_focus ="#020105"
+theme.menu_bg_focus ="#020105"
+theme.bg_normal = "#020105"
+theme.bg_focus = "#020105"
+theme.bg_urgent = "#020105"
+theme.fg_normal = "#666666"
+theme.fg_focus= "#6BBED2"
+theme.fg_urgent                                 = "#af1d18"
+theme.fg_minimize                               = "#ffffff"
+theme.border_width                              = 1
+theme.border_normal                             = "#1c2022"
+theme.border_focus                              = "#606060"
+theme.border_marked                             = "#3ca4d8"
+
+theme.menu_border_width                         = 0
+theme.menu_width                                = 130
+theme.menu_submenu_icon                         = theme.confdir .. "/icons/submenu.png"
+theme.menu_fg_normal                            = "#aaaaaa"
+theme.menu_fg_focus                             = "#ff8c00"
+theme.menu_bg_focus ="#020105"
+theme.menu_bg_focus ="#020105"
+
+theme.widget_temp                               = theme.confdir .. "/icons/temp.png"
+theme.widget_uptime                             = theme.confdir .. "/icons/ac.png"
+theme.widget_cpu                                = theme.confdir .. "/icons/cpu.png"
+theme.widget_weather                            = theme.confdir .. "/icons/dish.png"
+theme.widget_fs                                 = theme.confdir .. "/icons/fs.png"
+theme.widget_mem                                = theme.confdir .. "/icons/mem.png"
+theme.widget_fs                                 = theme.confdir .. "/icons/fs.png"
+theme.widget_note                               = theme.confdir .. "/icons/note.png"
+theme.widget_note_on                            = theme.confdir .. "/icons/note_on.png"
+theme.widget_netdown                            = theme.confdir .. "/icons/net_down.png"
+theme.widget_netup                              = theme.confdir .. "/icons/net_up.png"
+theme.widget_mail                               = theme.confdir .. "/icons/mail.png"
+theme.widget_batt                               = theme.confdir .. "/icons/bat.png"
+theme.widget_clock                              = theme.confdir .. "/icons/clock.png"
+theme.widget_vol                                = theme.confdir .. "/icons/spkr.png"
+theme.taglist_squares_sel                       = theme.confdir .. "/icons/square_a.png"
+theme.taglist_squares_unsel                     = theme.confdir .. "/icons/square_b.png"
+
+theme.color01= "#647C85"
+theme.color02= "#63919E"
+theme.color03= "#58ADC9"
+theme.color04= "#6BBED2"
+theme.color05= "#9EB0A6"
+theme.color06= "#A2D2DA"
+theme.color07= "#C8D2C8"
+theme.color08= "#666666"
+theme.color09= "#647C85"
+theme.color10= "#63919E"
+theme.color11= "#58ADC9"
+theme.color12= "#6BBED2"
+theme.color13= "#9EB0A6"
+theme.color14= "#A2D2DA"
+theme.color15= "#C8D2C8"
+
+
+theme.tasklist_plain_task_name                  = true
+theme.tasklist_disable_icon                     = true
+
+theme.useless_gap                               = 10
+
+theme.layout_tile                               = theme.confdir .. "/icons/tile.png"
+theme.layout_tilegaps                           = theme.confdir .. "/icons/tilegaps.png"
+theme.layout_tileleft                           = theme.confdir .. "/icons/tileleft.png"
+theme.layout_tilebottom                         = theme.confdir .. "/icons/tilebottom.png"
+theme.layout_tiletop                            = theme.confdir .. "/icons/tiletop.png"
+theme.layout_fairv                              = theme.confdir .. "/icons/fairv.png"
+theme.layout_fairh                              = theme.confdir .. "/icons/fairh.png"
+theme.layout_spiral                             = theme.confdir .. "/icons/spiral.png"
+theme.layout_dwindle                            = theme.confdir .. "/icons/dwindle.png"
+theme.layout_max                                = theme.confdir .. "/icons/max.png"
+theme.layout_fullscreen                         = theme.confdir .. "/icons/fullscreen.png"
+theme.layout_magnifier                          = theme.confdir .. "/icons/magnifier.png"
+theme.layout_floating                           = theme.confdir .. "/icons/floating.png"
+
+theme.titlebar_close_button_normal              = theme.confdir .. "/icons/titlebar/close_normal.png"
+theme.titlebar_close_button_focus               = theme.confdir .. "/icons/titlebar/close_focus.png"
+theme.titlebar_minimize_button_normal           = theme.confdir .. "/icons/titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus            = theme.confdir .. "/icons/titlebar/minimize_focus.png"
+theme.titlebar_ontop_button_normal_inactive     = theme.confdir .. "/icons/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive      = theme.confdir .. "/icons/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active       = theme.confdir .. "/icons/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active        = theme.confdir .. "/icons/titlebar/ontop_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive    = theme.confdir .. "/icons/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive     = theme.confdir .. "/icons/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active      = theme.confdir .. "/icons/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active       = theme.confdir .. "/icons/titlebar/sticky_focus_active.png"
+theme.titlebar_floating_button_normal_inactive  = theme.confdir .. "/icons/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive   = theme.confdir .. "/icons/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active    = theme.confdir .. "/icons/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active     = theme.confdir .. "/icons/titlebar/floating_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = theme.confdir .. "/icons/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = theme.confdir .. "/icons/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active   = theme.confdir .. "/icons/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active    = theme.confdir .. "/icons/titlebar/maximized_focus_active.png"
+
+return theme
